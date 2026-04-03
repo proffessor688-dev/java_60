2267aab16ae017cc4c618754e9e1a57119c98846
proffessor688-dev <proffessor688@gmail.com>
2026-04-03 17:07:52 +0530

    03/04/26

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+ class MathsUtilts{
+	public static int getFactorial(int num){
+		
+	  if(num==0 ||num==1){
+		  return 1;
+        } 
+		return num*getFactorial(num-1);
+	}
+ }
+ public class Factorial{
+	public static void main(String args[]){
+		int res=MathsUtilts.getFactorial(5);
+		System.out.println(res +" ");
+		
+	}
+ }
