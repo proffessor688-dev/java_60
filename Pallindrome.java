6870485f6c6860d0ad4a9c5ba4621c367a48f21b
proffessor688-dev <proffessor688@gmail.com>
2026-04-03 14:26:24 +0530

    3/04/26:

---
+++ Pallindrome.java
@@ -0,0 +1,27 @@
+import java.util.*;
+ class MathsUtility{
+	public static int checkPallindrome(int num){
+		int rev=0;
+		while(num!=0){
+			int digit=num%10;
+			rev=rev*10+digit;
+			num=num/10;
+		}
+		return rev;
+	}
+ }
+ public class Pallindrome{
+	 public static void main(String args[]){
+	  Scanner input=new Scanner(System.in);
+	    
+    	System.out.println("Enter the value :");
+        int num=input.nextInt();
+        int rev=MathsUtility.checkPallindrome(num);
+		if(num==rev){
+		 System.out.println(num +"is a Palindrome");
+		}else{
+			System.out.println(num + " is not a palindrome");
+		}
+        		
+	 }
+ }
