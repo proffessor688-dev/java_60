b4e11f8524066316f0740709bd57ca3bda596f57
proffessor688-dev <proffessor688@gmail.com>
2026-04-03 22:24:35 +0530

    03/04/26

--- MinMax.java
+++ MinMax.java
@@ -2,9 +2,15 @@ import java.util.*;
 
  class MathsUtils{
 	public static int[] getMinMax(int[] nums){
+		
+	if(nums.length == 0){
+        throw new IllegalArgumentException("Array is empty");
+    }
 	int max=nums[0];
 	int min=nums[0];
-    for(int i=0;i<nums.length;i++){
+	
+
+    for(int i=1;i<nums.length;i++){
 			if(nums[i]>max){
 			 max=nums[i];
 			}
