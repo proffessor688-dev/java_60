0c9aa57ef975fc07860f89d9ea042354fe9f3588
proffessor688-dev <proffessor688@gmail.com>
2026-04-03 22:12:53 +0530

    03/04

---
+++ MinMax.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+ class MathsUtils{
+	public static int[] getMinMax(int[] nums){
+	int max=nums[0];
+	int min=nums[0];
+    for(int i=0;i<nums.length;i++){
+			if(nums[i]>max){
+			 max=nums[i];
+			}
+			if(nums[i]<min){
+			 min=nums[i];	
+			}	
+		}
+		return new int[]{min,max};
+	}  
+}
+    public class MinMax{
+	  public static void main(String args[]){
+	  int nums[]={5,78,45,25,36,6,98};
+	  int[] results = MathsUtils.getMinMax(nums); 
+      for(int num:results){
+		System.out.println(num + " ");
+	  }	  
+    }
+ }
